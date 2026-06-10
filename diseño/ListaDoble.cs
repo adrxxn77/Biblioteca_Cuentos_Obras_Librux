054b08937c34c015e0339d4471e86175fa60b31c
adrxxn77 <N00426922@upn.pe>
2026-06-09 22:42:31 -0500

    Agregar implementación de NodoSimple, ListaSimple, NodoDoble y ListaDoble

---
+++ diseño/ListaDoble.cs
@@ -0,0 +1,59 @@
+﻿using Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace INTERFAZZZBIBLIOTECA
+{
+    public class ListaDoble
+    {
+        private Nodo cabeza;
+        private Nodo cola;
+
+        public void Agregar(string titulo, string autor)
+        {
+            Nodo nuevo = new Nodo(titulo, autor);
+            if (cabeza == null)
+            {
+                cabeza = cola = nuevo;
+            }
+            else
+            {
+                cola.sig = nuevo;
+                nuevo.ant = cola;
+                cola = nuevo;
+            }
+        }
+        public void Mostraradelante()
+        {
+            Nodo actual = cabeza;
+            while (actual != null)
+            {
+                Console.WriteLine($"{actual.titulo} - {actual.autor}");
+                actual = actual.sig;
+            }
+        }
+
+        //  Versión para interfaz gráfica
+        public void MostrarAdelante(ListBox lista)
+        {
+            lista.Items.Clear();
+
+            Nodo actual = cabeza;
+            if (actual == null)
+            {
+                lista.Items.Add("El catálogo está vacío.");
+                return;
+            }
+
+            while (actual != null)
+            {
+                lista.Items.Add($"{actual.titulo} - {actual.autor}");
+                actual = actual.sig;
+            }
+        }
+    }
+}// reemplazamos 
---
+++ diseño/NodoDoble.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public class Nodo
+    {
+        public string titulo;
+        public string autor;
+        public Nodo sig;
+        public Nodo ant;
+
+        public Nodo(string t, string a)
+        {
+            titulo = t;
+            autor = a;
+            sig = ant = null;
+        }
+    }
+} //reemplazamos
+
